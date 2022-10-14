2561686f67ab8944bd8efc6997f03795bb333420
Erik Westra <e.s.westra.95@gmail.com>
2022-10-14 14:46:07 +0200

    Disable .env loading and db connection until secrets are in place

--- main.go
+++ main.go
@@ -1,34 +1,31 @@
 package main
 
 import (
-	"log"
-	"os"
-
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 	"github.com/webstraservices/gateway/gateway"
-	"github.com/webstraservices/gateway/models"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Unable to load environment files - Err: %s", err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Unable to load environment files - Err: %s", err)
+	// }
 
-	connectionString := os.Getenv("DB_CONNECTION_STRING")
+	// connectionString := os.Getenv("DB_CONNECTION_STRING")
 
-	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
-	if err != nil {
-		log.Fatalf("Unable to open database instance")
-	}
+	// db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatalf("Unable to open database instance")
+	// }
 
-	db.AutoMigrate(models.User{})
+	// db.AutoMigrate(models.User{})
 
 	router := gin.Default()
-	router.Any("/*path", gateway.Gateway)
+	router.GET("/healthz", func(c *gin.Context) {
+		c.Writer.WriteHeader(200)
+	})
+
+	router.Any("/api/*path", gateway.Gateway)
 
 	router.Run(":3000")
 }
